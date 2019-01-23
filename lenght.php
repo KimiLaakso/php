964fdbf06e69d7a8abf838d64e06d7d88c25b4cb
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-01-23 14:20:28 +0200

    Add files via upload

---
+++ lenght.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Word lenght</title>
+	</head>
+	<body>
+		<form action="wordlenght.php" method="post">
+			Calculate length:<br>
+			<input type="text" name="wlenght">
+				<input type="submit" value="Send input!">
+			<br>
+			<br>
+
+		</form>
+</body>
+</html>
---
+++ wordlenght.php
@@ -0,0 +1,6 @@
+<?php
+		// get the values entered in the form
+		$word = $_POST['wlenght'];
+
+		echo ("Length of the word is". strlen($word));
+?>
